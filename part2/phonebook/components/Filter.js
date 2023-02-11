3c7086c8eaac567762ea9e8d79755f87105ca054
William Luna <42703238+William-Luna@users.noreply.github.com>
2023-02-11 14:57:30 -0800

    updating commit message

--- part2/phonebook/components/Filter.js
+++
@@ -1,7 +0,0 @@
-const Filter = ({ filterInput, handleFilterChange }) => {
-    return (
-        <div>Filter: Only show entries that contain <input value={filterInput} onChange={handleFilterChange} /> </div>
-    )
-}
-
-export default Filter
