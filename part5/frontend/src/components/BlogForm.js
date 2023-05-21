059ab6613c4e06de03d9cb8350776d2a4619dce3
William Luna <wluna98@gmail.com>
2023-05-20 18:04:14 -0700

    Exercise 5.16 Complete

--- part5/frontend/src/components/BlogForm.js
+++ part5/frontend/src/components/BlogForm.js
@@ -24,16 +24,16 @@ const BlogForm = ({ createBlog }) => {
       <h3>Create New Blog</h3>
       <form onSubmit={addBlog}>
         <div>
-                    Title:
-          <input type="text" value={title} name="title" onChange={({ target }) => setTitle(target.value)} />
+          Title:
+          <input type="text" value={title} name="title" onChange={({ target }) => setTitle(target.value)} placeholder='Blog Title' />
         </div><div>
-                    Author:
-          <input type="text" value={author} name="author" onChange={({ target }) => setAuthor(target.value)} />
+          Author:
+          <input type="text" value={author} name="author" onChange={({ target }) => setAuthor(target.value)} placeholder='Blog Author' />
         </div><div>
-                    URL:
-          <input type="text" value={url} name="url" onChange={({ target }) => setUrl(target.value)} />
+          URL:
+          <input type="text" value={url} name="url" onChange={({ target }) => setUrl(target.value)} placeholder='Blog Url' />
         </div>
-        <button type="submit">Create</button>
+        <button type="submit" className='submitButton'>Create</button>
       </form>
     </div>
   )
---
+++ part5/frontend/src/components/BlogForm.test.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import '@testing-library/jest-dom/extend-expect'
+import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
+import BlogForm from './BlogForm'
+
+test('form calls event handler and contains correct details for created blog', async () => {
+    const createBlog = jest.fn()
+    const user = userEvent.setup()
+
+    const { container } = render(<BlogForm createBlog={createBlog} />)
+
+    const inputTitle = screen.getByPlaceholderText('Blog Title')
+    const inputAuthor = screen.getByPlaceholderText('Blog Author')
+    const inputUrl = screen.getByPlaceholderText('Blog Url')
+    const submitButton = container.querySelector('.submitButton')
+
+    await user.type(inputTitle, 'Test Title')
+    await user.type(inputAuthor, 'Test Author')
+    await user.type(inputUrl, 'Test Url')
+
+    await user.click(submitButton)
+
+    expect(createBlog.mock.calls).toHaveLength(1)
+    expect(createBlog.mock.calls[0][0].title).toBe('Test Title')
+    expect(createBlog.mock.calls[0][0].author).toBe('Test Author')
+    expect(createBlog.mock.calls[0][0].url).toBe('Test Url')
+})
